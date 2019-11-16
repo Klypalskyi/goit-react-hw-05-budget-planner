3024161b91fb7d5fc4866af5b61919e0be53d225
Klypalskyi <klypalskyi@gmail.com>
2019-11-17 00:37:05 +0200

    Updates

---
+++ precache-manifest.e2d9776c616afac4a32d6c5d39ae99ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2ea36f0cf498baeb0b5675f1a50961",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "f010fc4e59844ccd74d7",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "b9e6d727f86c1e3357f0",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.f8b1366b.chunk.js"
+  },
+  {
+    "revision": "f010fc4e59844ccd74d7",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.05d448a2.chunk.js"
+  },
+  {
+    "revision": "55cd8204d1abadebe023",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.440ed870.js"
+  }
+]);
